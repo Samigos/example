089f98ca83bda81126a8574f97655447c129a000
Samigos <s.kaniras@gmail.com>
2016-10-14 21:07:15 +0300

    some changes

---
+++ gitExample2/List.c
@@ -0,0 +1,9 @@
+//
+//  List.c
+//  gitExample2
+//
+//  Created by Sotiris Kaniras on 14/10/16.
+//  Copyright © 2016 com.me. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ gitExample2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  gitExample2
+//
+//  Created by Sotiris Kaniras on 14/10/16.
+//  Copyright © 2016 com.me. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    printf("Some changes were made!\n");
+    
+    return 0;
+}
