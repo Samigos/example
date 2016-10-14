f266a1c7c77734bada83805286dc92b55c16ffa9
Samigos <s.kaniras@gmail.com>
2016-10-14 20:53:01 +0300

    llll

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gitExample2
+//
+//  Created by Sotiris Kaniras on 14/10/16.
+//  Copyright © 2016 com.me. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
